af6fceef8c6d6ef5a97909856613485e5a911858
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-03-10 13:35:53 +0530

    Insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,3 +1,17 @@
+import java.util.Arrays;
+
 public class InsertionSort {
-    
+    public static void main(String[] args) {
+        int arr[] = {8,3,5,1,9,4,2};
+        for(int i=1; i<arr.length; i++){
+            int k=arr[i];
+            int j = i;
+            while(j>0 && arr[j-1]>k){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = k;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
