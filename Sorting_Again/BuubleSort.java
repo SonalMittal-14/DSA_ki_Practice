cf907ad091299dfdea62f3f9d9d41f120dc8b8eb
Sonal Mittal <sonalmittal140103@gmail.com>
2024-09-11 01:31:30 +0530

    Bubble Sort

---
+++ Sorting_Again/BuubleSort.java
@@ -0,0 +1,29 @@
+// package Sorting_Again;
+import java.util.*;
+
+
+
+public class BuubleSort {
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        // System.out.println();
+    }
+
+    public static void main (String args[]){
+        int arr[] = {8,3,6,9,1,4,0};
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    // swap kar dena
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
